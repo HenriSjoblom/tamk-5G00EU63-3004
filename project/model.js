f09853e0b236da2dbf30f7ce3a7ef9b3a557d6d9
Henri Sjöblom <henri.sjoblom@tuni.fi>
2024-03-26 01:00:58 +0200

    syntax and error code check

--- project/model.js
+++ project/model.js
@@ -1,32 +1,32 @@
-const sqlite3 = require("sqlite3").verbose()
+const sqlite3 = require('sqlite3').verbose()
 
 const findTeam = (location, division, conference) => {
   return new Promise((resolve, reject) => {
-    const db = new sqlite3.Database("database.db")
+    const db = new sqlite3.Database('database.db')
 
     // Initialize an array to store the WHERE conditions
-    const whereConditions = [];
-    const queryParams = [];
+    const whereConditions = []
+    const queryParams = []
 
     if (location) {
-      whereConditions.push("LOWER(location) = ?")
+      whereConditions.push('LOWER(location) = ?')
       queryParams.push(location.toLowerCase())
     }
     if (division) {
-      whereConditions.push("LOWER(division) = ?")
+      whereConditions.push('LOWER(division) = ?')
       queryParams.push(division.toLowerCase())
     }
     if (conference) {
-      whereConditions.push("LOWER(conference) = ?")
+      whereConditions.push('LOWER(conference) = ?')
       queryParams.push(conference.toLowerCase())
     }
 
-    let whereStart= ""
+    let whereStart = ''
     if (queryParams.length > 0) {
-      whereStart = "WHERE "
+      whereStart = 'WHERE '
     }
     // Construct the complete WHERE clause
-    const whereClause = whereConditions.join(" AND ");
+    const whereClause = whereConditions.join(' AND ')
 
     const query = `SELECT id,
                           name,
@@ -41,38 +41,37 @@ const findTeam = (location, division, conference) => {
     db.all(query, queryParams, (err, rows) => {
       db.close()
       if (err) {
-          reject(err)
+        reject(err)
       } else {
-          resolve(rows)
+        resolve(rows)
       }
     })
   })
 }
 
-const findChampion = (season, team_id) => {
+const findChampion = (season, teamId) => {
   return new Promise((resolve, reject) => {
-
-    const db = new sqlite3.Database("database.db")
+    const db = new sqlite3.Database('database.db')
     // Initialize an array to store the WHERE conditions
-    const whereConditions = [];
-    const queryParams = [];
+    const whereConditions = []
+    const queryParams = []
 
     if (season) {
-      whereConditions.push("season = ?")
+      whereConditions.push('season = ?')
       queryParams.push(season)
     }
-    if (team_id) {
-      whereConditions.push("team_id = ?")
-      queryParams.push(team_id)
+    if (teamId) {
+      whereConditions.push('team_id = ?')
+      queryParams.push(teamId)
     }
 
-    let whereStart= ""
+    let whereStart = ''
     if (queryParams.length > 0) {
-      whereStart = "WHERE "
+      whereStart = 'WHERE '
     }
 
     // Construct the complete WHERE clause
-    const whereClause = whereConditions.join(" AND ")
+    const whereClause = whereConditions.join(' AND ')
 
     const query = `SELECT c.id,
                           c.season,
@@ -83,24 +82,23 @@ const findChampion = (season, team_id) => {
                          ON c.team_id == t.id
                    ${whereStart} ${whereClause}
                    ORDER BY c.id ASC;
-                   `;
+                   `
 
     db.all(query, queryParams, (err, rows) => {
       db.close()
       if (err) {
-          reject(err)
+        reject(err)
       } else {
-          resolve(rows)
+        resolve(rows)
       }
     })
   })
 }
 
-const createChampion = (id, season, team_id) => {
+const createChampion = (id, season, teamId) => {
   return new Promise((resolve, reject) => {
-
-    const db = new sqlite3.Database("database.db")
-    let queryParams = [id, season, team_id]
+    const db = new sqlite3.Database('database.db')
+    const queryParams = [id, season, teamId]
     const query = `INSERT INTO champion
                     (
                     id,
@@ -113,17 +111,17 @@ const createChampion = (id, season, team_id) => {
                     ?,
                     ?
                     );
-                    `;
+                    `
 
-    console.log("QUERY PARAMS " + queryParams)
-    console.log("QUERY: " + query)
+    console.log('QUERY PARAMS ' + queryParams)
+    console.log('QUERY: ' + query)
 
     db.all(query, queryParams, (err, rows) => {
       db.close()
       if (err) {
-          reject(err)
+        reject(err)
       } else {
-          resolve(rows)
+        resolve(rows)
       }
     })
   })
@@ -131,37 +129,36 @@ const createChampion = (id, season, team_id) => {
 
 const deleteChampion = (id) => {
   return new Promise((resolve, reject) => {
-
-    const db = new sqlite3.Database("database.db")
-    let queryParams = [id]
+    const db = new sqlite3.Database('database.db')
+    const queryParams = [id]
     const query = `DELETE
                    FROM champion
                    WHERE id = ?
-                    `;
+                    `
 
-    console.log("QUERY PARAMS " + queryParams)
-    console.log("QUERY: " + query)
+    console.log('QUERY PARAMS ' + queryParams)
+    console.log('QUERY: ' + query)
 
     db.all(query, queryParams, (err, rows) => {
       db.close()
       if (err) {
-          reject(err)
+        reject(err)
       } else {
-          resolve(rows)
+        resolve(rows)
       }
     })
   })
 }
 
-const findArena = (team_id) => {
+const findArena = (teamId) => {
   return new Promise((resolve, reject) => {
-    const db = new sqlite3.Database("database.db")
+    const db = new sqlite3.Database('database.db')
 
     let queryParams = []
-    let whereClause = ""
-    if (team_id) {
-      whereClause = "WHERE a.team_id = ?";
-      queryParams = [team_id]
+    let whereClause = ''
+    if (teamId) {
+      whereClause = 'WHERE a.team_id = ?'
+      queryParams = [teamId]
     }
 
     const query = `SELECT a.id,
@@ -173,53 +170,99 @@ const findArena = (team_id) => {
                          ON a.team_id == t.id
                    ${whereClause}
                    ORDER BY a.id ASC;
-                   `;
+                   `
 
     db.all(query, queryParams, (err, rows) => {
       db.close()
       if (err) {
-          reject(err)
+        reject(err)
       } else {
-          resolve(rows)
+        resolve(rows)
       }
     })
   })
 }
 
-const findFranchise = (location, division, conference, stanley_cups) => {
+const findArenaById = (id) => {
   return new Promise((resolve, reject) => {
-    const db = new sqlite3.Database("database.db")
+    const db = new sqlite3.Database('database.db')
 
+    const queryParams = [id]
+    const query = `SELECT id,
+                          name,
+                          team_id
+                   FROM arena
+                   WHERE id = ?
+                   `
 
-   // Initialize an array to store the WHERE conditions
-   const whereConditions = []
-   const queryParams = []
-   let havingClause = ""
+    console.log('PARAMS ' + queryParams)
+    console.log('QUERY: ' + query)
+    db.all(query, queryParams, (err, rows) => {
+      db.close()
+      if (err) {
+        reject(err)
+      } else {
+        resolve(rows)
+      }
+    })
+  })
+}
 
-   if (stanley_cups) {
-    havingClause = "HAVING stanley_cups > 0";
-  }
-   if (location) {
-     whereConditions.push("LOWER(location) = ?")
-     queryParams.push(location.toLowerCase());
-   }
-   if (division) {
-     whereConditions.push("LOWER(division) = ?")
-     queryParams.push(division.toLowerCase());
-   }
-   if (conference) {
-     whereConditions.push("LOWER(conference) = ?")
-     queryParams.push(conference.toLowerCase())
-   }
+const updateArena = (id, name, teamId) => {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database('database.db')
 
-   let whereStart= ""
-   if (queryParams.length > 0) {
-    whereStart = "WHERE "
-   }
-   // Construct the complete WHERE clause
-   const whereClause = whereConditions.join(" AND ")
+    const queryParams = [name, teamId, id]
+    const query = `UPDATE arena
+                   SET name = ?,
+                       team_id = ?
+                   WHERE id = ?
+                   `
 
-   const query = `SELECT t.id,
+    db.all(query, queryParams, (err, rows) => {
+      db.close()
+      if (err) {
+        reject(err)
+      } else {
+        resolve(rows)
+      }
+    })
+  })
+}
+
+const findFranchise = (location, division, conference, stanleyCup) => {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database('database.db')
+
+    // Initialize an array to store the WHERE conditions
+    const whereConditions = []
+    const queryParams = []
+    let havingClause = ''
+
+    if (stanleyCup) {
+      havingClause = 'HAVING stanley_cups > 0'
+    }
+    if (location) {
+      whereConditions.push('LOWER(location) = ?')
+      queryParams.push(location.toLowerCase())
+    }
+    if (division) {
+      whereConditions.push('LOWER(division) = ?')
+      queryParams.push(division.toLowerCase())
+    }
+    if (conference) {
+      whereConditions.push('LOWER(conference) = ?')
+      queryParams.push(conference.toLowerCase())
+    }
+
+    let whereStart = ''
+    if (queryParams.length > 0) {
+      whereStart = 'WHERE '
+    }
+    // Construct the complete WHERE clause
+    const whereClause = whereConditions.join(' AND ')
+
+    const query = `SELECT t.id,
                          t.name AS "team_name",
                          t.location,
                          t.division,
@@ -234,21 +277,20 @@ const findFranchise = (location, division, conference, stanley_cups) => {
                   ${whereStart} ${whereClause}
                   GROUP BY t.name
                   ${havingClause}
-                  ORDER BY t.id ASC;`;
+                  ORDER BY t.id ASC;`
 
-    console.log("WHERE CLAUSE " + whereClause)
-    console.log("QUERY: " + query)
+    console.log('WHERE CLAUSE ' + whereClause)
+    console.log('QUERY: ' + query)
     db.all(query, queryParams, (err, rows) => {
       db.close()
       if (err) {
-          reject(err)
+        reject(err)
       } else {
-          resolve(rows)
+        resolve(rows)
       }
     })
   })
 }
-
 
 module.exports = {
   findTeam,
@@ -256,5 +298,7 @@ module.exports = {
   createChampion,
   deleteChampion,
   findArena,
+  findArenaById,
+  updateArena,
   findFranchise
 }
--- project/server.js
+++ project/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-require('dotenv').config();
+const express = require('express')
+require('dotenv').config()
 
 const {
   findTeam,
@@ -7,30 +7,29 @@ const {
   createChampion,
   deleteChampion,
   findArena,
-  findFranchise,
-} = require('./model');
+  findArenaById,
+  updateArena,
+  findFranchise
+} = require('./model')
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
 const HTTP_STATUS_OK = 200
-const HTTP_STATUS_404 = 404
-const HTTP_STATUS_INTERNAL_ERROR = 500
+const POST_STATUS_CONFLICT = 409
+const STATUS_NOT_FOUND = 404
+const STATUS_INTERNAL_ERROR = 500
+const INTERNAL_SERVER_ERROR_MESSAGE = "Internal server error"
 
-
-app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain')
-  res.status(302).send('Hello World!\n')
-})
 
 app.get('/health', (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   res.status(HTTP_STATUS_OK).send('OK')
 })
 
-app.get('/api/v1/team', async(req, res) => {
+app.get('/api/v1/team', async (req, res) => {
   const location = req.query.location
   const division = req.query.division
   const conference = req.query.conference
@@ -39,119 +38,136 @@ app.get('/api/v1/team', async(req, res) => {
     const response = await findTeam(location, division, conference)
 
     if (response) {
-        res.json(response)
+      res.json(response)
+    } else {
+      res.status(STATUS_NOT_FOUND)
     }
   } catch (error) {
-    console.log(error)
-    res.status(HTTP_STATUS_INTERNAL_ERROR).json({message: "Something went wrong"})
+      console.log(error)
+      res.status(STATUS_INTERNAL_ERROR).json({ message: INTERNAL_SERVER_ERROR_MESSAGE })
   }
 })
 
-app.get('/api/v1/champion', async(req, res) => {
-    const season = req.query.season
-    const team_id = req.query.team_id
-    try {
-      const response = await findChampion(season, team_id)
-      if (response) {
-          res.json(response)
-      }
-    } catch (error) {
-      console.log(error)
-      res.status(HTTP_STATUS_INTERNAL_ERROR).json({message: "Something went wrong"})
+app.get('/api/v1/champion', async (req, res) => {
+  const season = req.query.season
+  const teamId = req.query.team_id
+  try {
+    const response = await findChampion(season, teamId)
+    if (response) {
+      res.json(response)
+    } else {
+      res.status(STATUS_NOT_FOUND)
     }
-  })
+  } catch (error) {
+    console.log(error)
+    res.status(STATUS_INTERNAL_ERROR).json({ message: INTERNAL_SERVER_ERROR_MESSAGE })
+  }
+})
 
-app.post('/api/v1/champion', async(req, res) => {
+app.post('/api/v1/champion', async (req, res) => {
   const id = req.body.id
   const season = req.body.season
-  const team_id = req.body.team_id
+  const teamId = req.body.team_id
   try {
-    const response = await createChampion(id, season, team_id)
+    const check = await findChampion(id)
+    if (check.length != 0) {
+      res.status(POST_STATUS_CONFLICT).json({ message: 'Already exists' })
+      return
+    }
+    const response = await createChampion(id, season, teamId)
     if (response) {
-        res.json(response)
+      res.json(response)
     }
   } catch (error) {
     console.log(error)
-    res.status(HTTP_STATUS_INTERNAL_ERROR).json({message: "Something went wrong"})
+    res.status(STATUS_INTERNAL_ERROR).json({ message: INTERNAL_SERVER_ERROR_MESSAGE })
   }
 })
 
-app.delete('/api/v1/champion/:id', async(req, res) => {
-
+app.delete('/api/v1/champion/:id', async (req, res) => {
   try {
-    const id = parseInt(req.params.id, 10);
-    const check = await findChampion(id);
+    const id = parseInt(req.params.id, 10)
+    const check = await findChampion(id)
     if (check.length === 0) {
-      res.status(404).json({ message: "Id not found" });
-      return;
+      res.status(STATUS_NOT_FOUND).json({ message: 'Id not found' })
+      return
     }
     const response = await deleteChampion(id)
     if (response) {
-        res.json(response)
+      res.json(response)
     }
   } catch (error) {
     console.log(error)
-    res.status(HTTP_STATUS_INTERNAL_ERROR).json({message: "Something went wrong"})
+    res.status(STATUS_INTERNAL_ERROR).json({ message: INTERNAL_SERVER_ERROR_MESSAGE })
   }
 })
 
-app.get('/api/v1/arena', async(req, res) => {
-  const team_id = req.query.team_id
+app.get('/api/v1/arena', async (req, res) => {
+  const teamId = req.query.team_id
   try {
-    const response = await findArena(team_id)
+    const response = await findArena(teamId)
     if (response) {
-        res.json(response);
+      res.json(response)
+    }else {
+      res.status(STATUS_NOT_FOUND)
     }
   } catch (error) {
     console.log(error)
-    res.status(HTTP_STATUS_INTERNAL_ERROR).json({message: "Something went wrong"})
+    res.status(STATUS_INTERNAL_ERROR).json({ message: INTERNAL_SERVER_ERROR_MESSAGE })
   }
 })
 
-app.get('/api/v1/franchise', async(req, res) => {
+app.patch('/api/v1/arena/:id', async (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const teamId = req.body.team_id
+  console.log('IN PATCH')
+  try {
+    console.log('ID: ' + id)
+    const check = await findArenaById(id)
+    if (check.length === 0) {
+      res.status(STATUS_NOT_FOUND).json({ message: 'Id not found' })
+      return
+    }
+    const response = await updateArena(id, name, teamId)
+    if (response) {
+      res.status().json(response)
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(STATUS_INTERNAL_ERROR).json({ message: INTERNAL_SERVER_ERROR_MESSAGE })
+  }
+})
+
+app.get('/api/v1/franchise', async (req, res) => {
   const location = req.query.location
   const division = req.query.division
   const conference = req.query.conference
   const stanleyCup = req.query.stanleyCup
-  console.log("stanley " + stanleyCup)
-  console.log("location " + location)
-  console.log("division " + division)
-  console.log("con " + conference)
+  console.log('stanley ' + stanleyCup)
+  console.log('location ' + location)
+  console.log('division ' + division)
+  console.log('con ' + conference)
 
   try {
     const response = await findFranchise(location, division, conference, stanleyCup)
     if (response) {
-        res.json(response)
+      res.json(response)
+    } else {
+      res.status(STATUS_NOT_FOUND)
     }
   } catch (error) {
     console.log(error)
-    res.status(HTTP_STATUS_INTERNAL_ERROR).json({message: "Something went wrong"})
+    res.status(STATUS_INTERNAL_ERROR).json({ message: INTERNAL_SERVER_ERROR_MESSAGE })
   }
 })
 
-
-    /*
-    // https://expressjs.com/en/guide/routing.html#route-parameters
-    app.get('/search/last/:last', (req, res) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(302).send(`${JSON.stringify(req.params)}\n`);
-    })
-
-    app.get('/query', (req, res) => {
-	let first = req.query.first;
-	let last = req.query.last;
-	let name = first + ' ' + last;
-	res.setHeader('Content-Type', 'application/json');
-	res.status(302).send(`${JSON.stringify(req.query)}\n`);
-    })
-    */
-
-app.use((req, res) => {	// Default: any other request
-    res.setHeader('Content-Type', 'application/json');
-    res.status(HTTP_STATUS_404).json({});
-});
-
+// Default: any other request
+app.use((req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.status(STATUS_NOT_FOUND).json({ message: 'Endpoint not found' })
+})
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
